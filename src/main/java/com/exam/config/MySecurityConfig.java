3e608692155fc46f9e0428ef5654b63486a01d1c
ks <kartikeyasoft@mail.com>
2025-01-03 09:01:05 +0530

    pom updated

--- src/main/java/com/exam/config/MySecurityConfig.java
+++ src/main/java/com/exam/config/MySecurityConfig.java
@@ -64,7 +64,7 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .cors()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/generate-token", "/user/").permitAll()
+                .antMatchers("/generate-token", "/user/", "/test/**").permitAll()
                 .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .anyRequest().authenticated()
                 .and()
---
+++ src/main/java/com/exam/controller/Test.java
@@ -0,0 +1,26 @@
+package com.exam.controller;
+
+import com.exam.model.exam.Category;
+import com.exam.model.exam.Quiz;
+import com.exam.service.QuizService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/test")
+public class Test { 
+
+    //get active quizzes
+    @GetMapping
+    public String getActiveQuizzes() {
+        return " Server Working ";
+    }
+
+}
